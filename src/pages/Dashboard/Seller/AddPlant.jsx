29d0ceb0c41e34a67236eec2408b9abae14625fd
SHAHAB UDDIN <shahab.82946@gmail.com>
2025-01-13 19:15:08 +0600

    add plant component creaed and addplant form created and handlesubmit function made and passed as props throug addplant to addplantform, accet function in addpalnt form and set onsubmit button

--- src/pages/Dashboard/Seller/AddPlant.jsx
+++ src/pages/Dashboard/Seller/AddPlant.jsx
@@ -1,7 +1,32 @@
 import { Helmet } from 'react-helmet-async'
 import AddPlantForm from '../../../components/Form/AddPlantForm'
+import useAuth from '../../../hooks/useAuth'
+import { imageUpload } from '../../../API/utils'
 
 const AddPlant = () => {
+    const {user} = useAuth()
+    const handleSubmit = async e => {
+        e.preventDefault()
+        const form = e.targal;
+        const name = form.name.value;
+        const description = form.description.value;
+        const price = parseFloat(form.price.value);
+        const quantity = parseInt(form.quantity.value);
+        const image = form.image.files[0]
+        const imageUrl = await imageUpload(image)
+
+        // seller info
+        const seller = {
+            name: user?.name,
+            image: user?.photoURL,
+            email: user?.email
+        }
+
+        // product infot with seller info==
+        const plantSeller = {
+            name, price, description, quantity, image: imageUrl, seller
+        }
+    }
   return (
     <div>
       <Helmet>
